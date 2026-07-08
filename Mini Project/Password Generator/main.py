8aec08ecf5f225f8d967ed880af8c7a9bb08110c
Shreya Gade <shreyagade2172@gmail.com>
2026-07-08 21:24:45 +0530

    Add Password Generator project
    Created a simple Password Generator using Python. It generates a random password based on the length entered by the user using letters, numbers, and symbols.

---
+++ Mini Project/Password Generator/main.py
@@ -0,0 +1,10 @@
+import random
+
+length = int(input("Enter password length: "))
+
+password = ""
+
+for i in range(length):
+    password += random.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-+")
+
+print("Generated password:", password)
