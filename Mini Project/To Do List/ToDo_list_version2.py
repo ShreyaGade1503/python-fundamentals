80474861a9fe57061eefd88c02e40cdd061654ce
Shreya Gade <shreyagade2172@gmail.com>
2026-08-24 19:23:41 +0530

    Add file handling for To-Do List

--- Mini Project/To Do List/ToDo_list_version2.py
+++ Mini Project/To Do List/ToDo_list_version2.py
@@ -1,27 +1,23 @@
-
-tasks = [ 
-    "Solve maths problems",
-    "Write a poem",
-    "Create a short story",
-]
-
-
-
 def load_tasks():
     with open("tasks.txt", "r") as f:
         tasks = f.read().splitlines()
     return tasks
+
 
 def save_tasks():
     with open("tasks.txt", "w") as f:
         for task in tasks:
             f.write(task + "\n")
 
+
 def add_task():
     task = input("Enter task to add : ")
+
     tasks.append(task)
     save_tasks()
-    print(" Task added successfully! \n ")
+
+    print(" Task added successfully! \n")
+
 
 def remove_task():
     if len(tasks) == 0:
@@ -34,23 +30,30 @@ def remove_task():
 
     pop_task = int(input("Enter task number to remove : "))
 
-    if pop_task <= len(tasks) :
+    if pop_task <= len(tasks):
         index = pop_task - 1
         tasks.pop(index)
+        save_tasks()
 
     print(" Task Removed Successfully! \n")
+
 
 def view_task():
     if len(tasks) == 0:
         print(" No tasks available.")
         return
+
     print("\nYour Tasks:")
     for index, task in enumerate(tasks, start=1):
-        print(index , task)
+        print(index, task)
+
 
 def Exit():
     print(" Exiting To Do List \n")
-    
+
+
+tasks = load_tasks()
+
 
 while True:
 
@@ -60,18 +63,19 @@ while True:
     print("4. Exit")
 
     choice = int(input("Enter choice : "))
-    if choice == 1 :
+
+    if choice == 1:
         add_task()
-    elif choice == 2 :
+
+    elif choice == 2:
         remove_task()
-    elif choice == 3 :
+
+    elif choice == 3:
         view_task()
-    elif choice == 4 :
+
+    elif choice == 4:
         Exit()
         break
+
     else:
         print(" Invalid Choice!! ")
-
-
-
-
